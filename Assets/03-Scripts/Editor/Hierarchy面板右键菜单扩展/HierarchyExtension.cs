65dde4fe1b5a28e5735b6fd2ab285bbcec292cf7
Fly Rabbit <2514060610@qq.com>
2026-01-05 18:55:42 +0800

    添加了部分菜单创建项

---
+++ Assets/03-Scripts/Editor/Hierarchy面板右键菜单扩展/HierarchyExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEditor;
+public class HierarchyExtension : Editor
+{
+    /*
+     * 只要根目录是"GameObject",他就会出现在Hierarchy面板的右键菜单里
+     * 此时，你的调用方法可以加一个名为MenuCommand的参数。
+     * 因为Hierarchy面板的右键菜单里内容过多，可以通过参数调整一下菜单项所在的位置，这里就不详细解释了
+     * 
+     */
+
+    [MenuItem("GameObject/Sample/Create Empty", false, 10)]
+    public static void CreateEmpty(MenuCommand menuCommand)
+    {
+        //创建新物体
+        GameObject gameObject = new GameObject("GameObject");
+        //设置父节点
+        GameObjectUtility.SetParentAndAlign(gameObject, menuCommand.context as GameObject);
+        //支持撤销操作
+        Undo.RegisterCreatedObjectUndo(gameObject, "Create Game Object");
+        //选中物体
+        Selection.activeObject = gameObject;
+    }
+}
---
+++ Assets/03-Scripts/Editor/顶部菜单栏扩展/TopMenuBarExtension.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+public class TopMenuBarExtension : Editor
+{
+    /*
+     * 使用MenuItem特性即可在顶部菜单栏添加一个选项。
+     * 点击此选项，便可执行特定的方法，需要注意的是，该方法必须是静态方法。
+     */
+
+    [MenuItem("Sample/顶部菜单栏/打开持久化存储文件夹")]
+    public static void OpenPersistentDataFolder()
+    {
+        EditorUtility.RevealInFinder(Application.persistentDataPath);
+    }
+
+
+    [MenuItem("Sample/顶部菜单栏/显示选中物体的名称")]
+    public static void ShowSelectedGameObjectName()
+    {
+        string name = Selection.activeGameObject.name;
+        EditorUtility.DisplayDialog("选中物体", $"你选中的物体的名称为{name}", "ok");
+    }
+
+    /*
+     * 你可以给一个菜单项添加验证方法，如果此方法不返回true，这个菜单选项就是灰色的，不能选中。
+     * 这里的范例是你必须选中了一个物体才能验证通过。
+     */
+
+    [MenuItem("Sample/顶部菜单栏/显示选中物体的名称", true)]
+    public static bool ShowSelectedGameObjectName_Validate()
+    {
+        if (Selection.activeGameObject == null)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
